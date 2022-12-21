5446f0e8436da5ea77e30cbcde8113f4723eb9c2
Glitch (glitch-in-bio) <none>
2022-12-21 17:09:38 +0000

    ☎️🏤 Checkpoint ./rss-build.js:1908428/748 ./package.json:1908428/2124

--- rss-build.js
+++ rss-build.js
@@ -1,4 +1,16 @@
 var fs = require('fs');
+var nunjucks = require('nunjucks');
+var settings = require('settings.json');
 
-fs.readFile('feed.json', 'utf8', 
-fs.writeFile('myjsonfile.json', json, 'utf8', callback);
+module.exports = function(){
+  nunjucks.configure({ autoescape: true });
+  let feedJson = fs.readFileSync('feed.json', 'utf8');
+
+  let json = JSON.parse(feedJson);
+
+  let itemStrings = settings.links.map((link) => JSON.stringify(link));
+  let feedStrings = json.items.map(link => JSON.stringify(link))
+  let uniqueItems = new Set(...feedStrings ...)
+
+  fs.writeFileSync('myjsonfile.json', json, 'utf8');
+}
