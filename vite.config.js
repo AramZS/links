cbc92b5e1ddb411b9218c98162e57ef0087651ab
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2025-06-30 18:24:30 -0400

    Updating this Glitch project to try and run elsewhere

--- vite.config.js
+++ vite.config.js
@@ -2,36 +2,42 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 // Slightly modified from https://github.com/alexlafroscia/vite-plugin-handlebars
 import handlebars from "@glitchdotcom/vite-plugin-handlebars";
-import rssBuild from "./rss-build"
+import rssBuild from "./rss-build";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ command, mode }) => {
-  console.log('BUILDING from config')
-  
-  return {
-    plugins: [
-      rssBuild(),
-      handlebars({
-        partialDirectory: resolve(__dirname, "layout"),
-        settingsFile: 'settings.json',
-        helpers: {
-          hostasclass: value => new URL(value).hostname.replace(/\./g, "_")
-        },
-        reloadOnPartialChange: true
-      })
-    ],
-    build: {
-      cssCodeSplit: false,
-      outDir: "build"
-    },
-    optimizeDeps: {
-      exclude: ['./settings.json']
-    },
-    server: {
-      strictPort: true,
-      hmr: {
-        port: 443
-      }
-    }
-  };
+	console.log("BUILDING from config");
+
+	return {
+		plugins: [
+			rssBuild(),
+			handlebars({
+				partialDirectory: resolve(__dirname, "layout"),
+				settingsFile: "settings.json",
+				helpers: {
+					hostasclass: (value) =>
+						new URL(value).hostname.replace(/\./g, "_"),
+				},
+				reloadOnPartialChange: true,
+			}),
+		],
+		build: {
+			cssCodeSplit: false,
+			outDir: "build",
+		},
+		optimizeDeps: {
+			exclude: ["./settings.json"],
+		},
+		server: {
+			strictPort: false,
+			port: 8085,
+			hmr: {
+				port: 443,
+			},
+		},
+		preview: {
+			strictPort: false,
+			port: 8084,
+		},
+	};
 });
